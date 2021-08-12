f545854e0cab82d5dfc470328102050602fab981
s s <w@w.com>
2021-08-11 22:05:11 -0700

    Pushed from Domino: /u/wadkars/ON_DEMAND_SPARK/runs/6114a831b7181f12fed29008

--- scripts/.ipynb_checkpoints/runpi-remote-from-ondemand-checkpoint.sh
+++ scripts/.ipynb_checkpoints/runpi-remote-from-ondemand-checkpoint.sh
@@ -1,7 +1,15 @@
 export SPARK_CONF_DIR=$SPARK_CONF_DIR_REMOTE
 export SPARK_HOME=$SPARK_HOME_REMOTE
-PATH=$SPARK_HOME/bin:$PATH
-hdfs dfs -rmr /user/dominospark/mypi/
+export HADOOP_HOME=$HADOOP_HOME_REMOTE
+export HADOOP_YARN_HOME=$HADOOP_YARN_HOME_REMOTE
+export HADOOP_HDFS_HOME=$HADOOP_HDFS_HOME_REMOTE
+export HADOOP_MAPRED_HOME=$HADOOP_HOME_REMOTE
+export HADOOP_CONF_DIR=$HADOOP_CONF_DIR_REMOTE
+PATH=$HADOOP_HOME/bin:$HADOOP_HDFS_HOME/bin:$SPARK_HOME/bin:$PATH
+#hdfs dfs -rmr /user/dominospark/mypi/
+echo $2
+hdfs dfs -rmr $2
 export PYSPARK_PYTHON=/usr/bin/python3
 export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
-$SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster ../python/calcpi.py /user/dominospark/mypi/
+#$SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster ../python/calcpi.py /user/dominospark/mypi/
+$SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster ../python/calcpi.py $1 $2 
--- scripts/runpi-remote-from-ondemand.sh
+++ scripts/runpi-remote-from-ondemand.sh
@@ -1,14 +1,17 @@
 export SPARK_CONF_DIR=$SPARK_CONF_DIR_REMOTE
 export SPARK_HOME=$SPARK_HOME_REMOTE
+
 export HADOOP_HOME=$HADOOP_HOME_REMOTE
 export HADOOP_YARN_HOME=$HADOOP_YARN_HOME_REMOTE
 export HADOOP_HDFS_HOME=$HADOOP_HDFS_HOME_REMOTE
 export HADOOP_MAPRED_HOME=$HADOOP_HOME_REMOTE
 export HADOOP_CONF_DIR=$HADOOP_CONF_DIR_REMOTE
-PATH=$HADOOP_HDFS_HOME/bin:$SPARK_HOME/bin:$PATH
+
+PATH=$HADOOP_HOME/bin:$HADOOP_HDFS_HOME/bin:$SPARK_HOME/bin:$PATH
 #hdfs dfs -rmr /user/dominospark/mypi/
+echo $2
 hdfs dfs -rmr $2
 export PYSPARK_PYTHON=/usr/bin/python3
 export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
-#$SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster ../python/calcpi.py hdfs://10.0.1.76:8020 /user/dominospark/mypi/
+#$SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster ../python/calcpi.py /user/dominospark/mypi/
 $SPARK_HOME/bin/spark-submit --master yarn --deploy-mode cluster ../python/calcpi.py $1 $2 
