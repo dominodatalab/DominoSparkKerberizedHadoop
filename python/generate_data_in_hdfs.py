702525a4a13bfe0675d9cc3799298888a6b4e52d
s s <w@w.com>
2021-08-13 11:32:36 -0700

    Pushed from Domino: /u/wadkars/ON_DEMAND_SPARK/runs/611676dfb7181f12fed2a514

--- python/generate_data_in_hdfs.py
+++ python/generate_data_in_hdfs.py
@@ -2,27 +2,35 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, DoubleType,IntegerType
 import random
 import sys
-cnt=int(sys.argv[1])
-hdfs_endpoint = sys.argv[2] #hdfs://10.0.1.76:8020
-hdfs_path = sys.argv[3]
-sparkSession = SparkSession.builder.appName("Generate Data") \
-.config("spark.dynamicAllocation.enabled", "false") \
-.config("fs.default.name", hdfs_endpoint) \
-.getOrCreate()
-sc=sparkSession.sparkContext
-import random
-#Create datasets
-data = []
 
-for i in range(cnt):
-    data.append((i,random.randint(1,100),random.randint(1,100),random.randint(1,100)))
+def main(args):
+    cnt=args[0]
+    hdfs_endpoint = args[1] #hdfs://10.0.1.76:8020
+    hdfs_path = args[2]
+    master='yarn'
+    if(args.length>3):
+        master = args[3]
+    sparkSession = SparkSession.builder.appName("Generate Data") \
+    .config("master",master)
+    .config("spark.dynamicAllocation.enabled", "false") \
+    .config("fs.default.name", hdfs_endpoint) \
+    .getOrCreate()
+    sc=sparkSession.sparkContext
+    import random
+    #Create datasets
+    data = []
 
-columns = StructType([ StructField("id", IntegerType(), True),
-                       StructField("v1", IntegerType(), True),
-                       StructField("v2", IntegerType(), True),
-                       StructField("v3", IntegerType(), True) ])
+    for i in range(cnt):
+        data.append((i,random.randint(1,100),random.randint(1,100),random.randint(1,100)))
 
+    columns = StructType([ StructField("id", IntegerType(), True),
+                           StructField("v1", IntegerType(), True),
+                           StructField("v2", IntegerType(), True),
+                           StructField("v3", IntegerType(), True) ])
 
-df = sparkSession.createDataFrame(data=data, schema=columns)
-df.write.csv(hdfs_path)
-sc.stop()
+    df = sparkSession.createDataFrame(data=data, schema=columns)
+    df.write.csv(hdfs_path)
+    sc.stop()
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
